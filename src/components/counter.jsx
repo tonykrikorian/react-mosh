c89a8d8da63127194efd62b305175677844116f9
Tony <tony.krikorian@outlook.com>
2019-05-16 22:22:34 -0400

    Renderiza listas de forma dinamica

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 2,
+    tags:['tag1','tag2','tag3']
   };
   render() {
-      let classes = "badge m-2 badge-";
-      classes += (this.state.count === 0)? "warning" : "primary";
     return (
       <React.Fragment>
-        <span className={classes}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            {this.state.tags.map(tag => <li>{tag}</li>)}
+        </ul>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "zero" : count;
