a3fdc3dacbd3aec08660e240dde812f93afe0847
Tony <tony.krikorian@outlook.com>
2019-05-16 21:40:38 -0400

    Primer Commit React App

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World React</h1>
+      </div>
+    );
+  }
+}
+
+export default Counter;
