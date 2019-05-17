7736a508cbe5d01e8b1626edb6b4c4139b634376
Tony <tony.krikorian@outlook.com>
2019-05-16 23:12:56 -0400

    Actualiza el estado del DOM

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,36 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 2,
-    tags:['tag1','tag2','tag3']
+    count: 0,
+    tags: []
   };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
-  renderTags(){
-      if (this.state.tags.length === 0) return <p>There are no tags</p>
-      return <ul>{this.state.tags.map(tag =><li key={tag}>{tag}</li>)}</ul>;
+  handleIncrement = () => {
+  this.setState({count: ++this.state.count})
   }
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-         {this.renderTags()}
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <br />
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
