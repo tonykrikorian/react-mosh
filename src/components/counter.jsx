e0fe465b57ee9cf298b8458b4e4d94ce63643de3
Tony <tony.krikorian@outlook.com>
2019-05-16 22:14:23 -0400

    Añade clases de forma dinamica a los elementos

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,9 +5,11 @@ class Counter extends Component {
     count: 0
   };
   render() {
+      let classes = "badge m-2 badge-";
+      classes += (this.state.count === 0)? "warning" : "primary";
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
