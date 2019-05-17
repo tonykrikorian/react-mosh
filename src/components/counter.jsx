ac10cc39c22a463b820f00186bde8ecdb06be3cc
Tony <tony.krikorian@outlook.com>
2019-05-16 22:12:22 -0400

    Añade clases a los elementos

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
   render() {
     return (
-      <div>
-        <h1>Hello World React</h1>
-      </div>
+      <React.Fragment>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
   }
 }
 
